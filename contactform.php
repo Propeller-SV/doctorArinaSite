535ef50c53b61d9d8b9c776d816a2e621b381da2
Serhii Prokhorenko <grey.psv@gmail.com>
2015-09-10 12:37:53 +0300

    In contactfrom. Change id=contactform to class. Remove values of phone and message inputs. Replace <a> with input type=submit

--- contactform.php
+++ contactform.php
@@ -1,14 +1,15 @@
-<form method="post" id="contactform">
+<form method="post" class="contactform">
 	<div class="input-group-lg">
 		<input type="text" name="cf_name" class="form-control" value="<?= isset( $_POST["cf_name"] ) ? esc_attr( $_POST["cf_name"] ) : ''; ?>" placeholder="Ваше Имя" required>
 	</div>
 	<div class="input-group-lg">
-		<input type="text" name="cf_phone" class="form-control" value="<?= isset( $_POST["cf_phone"] ) ? esc_attr( $_POST["cf_phone"] ) : ''; ?>" placeholder="Ваш телефон" required>
+		<input type="text" name="cf_phone" class="form-control" value="" placeholder="Ваш телефон" required>
 	</div>
 	<div class="comments">
-		<textarea type="text" name="cf_message" class="form-control" placeholder="Опишите Вашу проблему" required><?= isset( $_POST["cf_message"] ) ? esc_attr( $_POST["cf_message"] ) : ''; ?></textarea>
+		<textarea type="text" name="cf_message" rows="4" class="form-control" placeholder="Опишите Вашу проблему" required></textarea>
 	</div>
-	<input class="hidden" type="submit" name="cf_submit" id="cf_submit">
-	<p class="text-center"><a href="javascript:{}" onclick="document.getElementById('cf_submit').click()">Отправить</a></p>
+	<div class="text-center">
+		<input type="submit" value="Отправить" class="submit" name="cf_submit">
+	</div>
 </form>
 <p>*Информация останется конфиденциальной</p>
